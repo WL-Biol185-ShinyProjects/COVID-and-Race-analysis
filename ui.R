3649e3683dd0933c218a6e1713f575381040659b
Carissa Rodriguez <rodriguezc21@mail.wlu.edu>
2020-10-22 13:03:41 -0400

    updated tab names and deleted icons

--- ui.R
+++ ui.R
@@ -1,18 +1,23 @@
 library(shinydashboard)
+#source pages here
 
 dashboardPage(
   dashboardHeader(title = "COVID-19 Case Explorer"),
   dashboardSidebar(
     sidebarMenu(
-    menuItem("Introduction", tabName = "Introduction", icon = icon("Introduction")),
-    menuItem("Cases & Deaths", tabName = "Cases & Deaths", icon = icon("Cases & Deaths")),
-    menuItem("Political Climate", tabName = "Political Climate", icon = icon("Political Climate")),
-    menuItem("Demographics", tabName = "Demographics", icon = icon("Demographics")),
-    menuItem("Data Explorer", tabName = "Data Explorer", icon = icon("Data Explorer"))
+    menuItem("Introduction", tabName = "Introduction"),
+    menuItem("Cases & Deaths", tabName = "CasesDeaths"),
+    menuItem("Political Climate", tabName = "PoliticalClimate"),
+    menuItem("Demographics", tabName = "Demographics"),
+    menuItem("Data Explorer", tabName = "DataExplorer")
   )
   ),
   ## Body content
   dashboardBody(
-    
+    tabItems(
+      tabItem("Introduction", "Introduction goes here"),
+      tabItem("CasesDeaths", "graphs go here")
+      
+    )
   )
 )
