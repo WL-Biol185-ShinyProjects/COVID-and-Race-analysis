d06823ade5efbb59553c0a5a413d0e0446c06053
Hannah Choi <choih21@mail.wlu.edu>
2020-10-22 13:09:24 -0400

    fix

--- server.R
+++ server.R
@@ -2,16 +2,5 @@ library(shinydashboard)
 
 # Define server logic required to plot various variables with COVID cases and deaths
 function(input, output) {
-<<<<<<< HEAD
 
-  }
-=======
-    set.seed(122)
-    histdata <- rnorm(500)
-    
-    output$plot1 <- renderPlot({
-      data <- histdata[seq_len(input$slider)]
-      hist(data)
-    })
-  }
->>>>>>> f11927d629a5fbcd353c2737987fef9bc47e98df
+}
