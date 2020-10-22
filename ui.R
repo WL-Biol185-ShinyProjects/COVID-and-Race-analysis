4d64b1d3eacf69d436ef3c6ecf4652fc407b8427
Carissa Rodriguez <rodriguezc21@mail.wlu.edu>
2020-10-22 12:52:19 -0400

    deleted tab items from ui because we have separate r scripts for each tab

--- ui.R
+++ ui.R
@@ -13,23 +13,6 @@ dashboardPage(
   ),
   ## Body content
   dashboardBody(
-    tabItems(
-      # First tab content
-      tabItem(tabName = "dashboard",
-              fluidRow(
-                box(plotOutput("plot1", height = 250)),
-                
-                box(
-                  title = "Controls",
-                  sliderInput("slider", "Number of observations:", 1, 100, 50)
-                )
-              )
-      ),
-      
-      # Second tab content
-      tabItem(tabName = "widgets",
-              h2("Widgets tab content")
-      )
-    )
+    
   )
 )
