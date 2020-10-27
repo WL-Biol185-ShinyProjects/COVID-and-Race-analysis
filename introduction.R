92aad5fb9a0db937655627174b602e08bd7bd225
Carissa Rodriguez <rodriguezc21@mail.wlu.edu>
2020-10-27 12:22:59 -0400

    adding boxes

--- introduction.R
+++ introduction.R
@@ -2,7 +2,13 @@ tabItems(
   # Introduction Page
   tabItem(tabName = "Introduction",
           fluidRow(
-            box(introduction.md
-            ))
+            box(plotOutput("plot1")),
+            
+            box(
+              "Box content here", br(), "More box content",
+              sliderInput("slider", "Slider input:", 1, 100, 50),
+              textInput("text", "Text input:")
+            )
+          )
   )
-)
+)  
