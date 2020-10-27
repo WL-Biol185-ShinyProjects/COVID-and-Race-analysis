89844f585248ef2f39037fe2ccf9d2cd568c88d1
Carissa Rodriguez <rodriguezc21@mail.wlu.edu>
2020-10-27 12:58:49 -0400

    intro md is not working

--- introduction.R
+++ introduction.R
@@ -1,12 +1,3 @@
 library(shiny)
 library(markdown)
 
-fluidPage(
-  
-  titlePanel("Coronavirus 2019: An Overview"),
-  fluidRow(
-    column(4,
-           includeMarkdown(introduction.md)
-    )
-  )
-)
