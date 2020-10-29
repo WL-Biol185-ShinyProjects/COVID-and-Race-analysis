9630e36a2a26c74f349b98e945ce982aa4b66646
Hannah Choi <choih21@mail.wlu.edu>
2020-10-29 12:37:18 -0400

    put back james code

--- political-climate.R
+++ political-climate.R
@@ -3,3 +3,16 @@ library(leaflet)
 library(rgdal)
 library(ggplot2)
 
+
+  #in this page, we'll clean up data and assign it to variables that can be referenced in the server and ui R pages.
+  a <- read.csv("1976-2016-president.csv")
+b <- read.csv("1976-2016-president.csv")
+
+years <- list(year2016 = a,
+              year2020 = b)
+
+xyz <- renderLeaflet({
+  data <- years[[input$year]]
+  leaflet(data)
+})
+
