2021ee54de8fc0bd6a49082a9e9d9a2fafad0754
Carissa Rodriguez <rodriguezc21@mail.wlu.edu>
2020-11-05 13:23:16 -0500

    made unused columns in large RDS file null

---
+++ rsdNullColumns.R
@@ -0,0 +1,7 @@
+CDCcovidData <- readRDS("COVIDsurveillancedata.RDS")
+CDCcovidData$cdc_report_dt = NULL
+CDCcovidData$onset_dt = NULL
+CDCcovidData$current_status = NULL
+CDCcovidData$hosp_yn = NULL
+CDCcovidData$icu_yn = NULL
+CDCcovidData$medcond_yn = NULL
