57343d50e64e6dd4865328d7a54108e4d700a1a8
Hannah Choi <choih21@mail.wlu.edu>
2020-11-05 12:51:19 -0500

    adding my data explorerer code

--- data-explorer.R
+++ data-explorer.R
@@ -3,8 +3,20 @@ library(leaflet)
 library(tidyverse)
 library(rgdal)
 
-totalData <- read_csv("United_States_COVID-19_Cases_and_Deaths_by_State_over_Time.csv")
-totalData$detection_date <- as.Date(totalData$submission_date, format = "%m/%d/%Y")
+totalCases <- read_csv("United_States_COVID-19_Cases_and_Deaths_by_State_over_Time.csv")
+totalCases$detection_date <- as.Date(totalCases$submission_date, format = "%m/%d/%Y")
 states <- read_csv("states")
 colnames(states) <- c("StateName", "Abbreviation")
-totalData <- left_join(totalData, states, by = c("state" = "Abbreviation"))
+totalCases <- left_join(totalCases, states, by = c("state" = "Abbreviation"))
+
+totalDeaths <- read_csv("United_States_COVID-19_Cases_and_Deaths_by_State_over_Time.csv")
+totalDeaths$detection_date <- as.Date(totalDeaths$submission_date, format = "%m/%d/%Y")
+states <- read_csv("states")
+colnames(states) <- c("StateName", "Abbreviation")
+totalDeaths <- left_join(totalDeaths, states, by = c("state" = "Abbreviation"))
+
+dataexplorer <- 
+  fluidPage(
+    width = 12,
+    leafletOutput("DataExplorer")
+  )
