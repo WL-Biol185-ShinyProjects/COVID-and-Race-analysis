dbe3b6d28c378d328369e935d373cc61c6523745
James Ricks <ricksj21@mail.wlu.edu>
2020-11-12 12:56:54 -0500

    refined bin values

--- server.R
+++ server.R
@@ -83,7 +83,8 @@ function(input, output) {
                           )
                         })
     #binning colors
-    bins <- c(0, 0.2, 0.4, 0.6, 0.8, 1, Inf)
+    #bins <- c(-1, -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0)
+    bins <- c(-.3, -.25, -0.2, -.15, -0.1, -.05, 0)
     statePoliData <- do.call(rbind, tableList)
     
     countryMap <- rgdal::readOGR("states.geo.json")
