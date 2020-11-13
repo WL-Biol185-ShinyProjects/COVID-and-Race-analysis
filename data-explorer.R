da25ab1e0a025ac08745611361b57e94cb0e00c7
Hannah Choi <choih21@mail.wlu.edu>
2020-11-13 17:40:32 -0500

    data explorer page variable changes

--- data-explorer.R
+++ data-explorer.R
@@ -4,23 +4,14 @@ library(tidyverse)
 library(rgdal)
 library(lubridate)
 
-cases <- read_csv("United_States_COVID-19_Cases_and_Deaths_by_State_over_Time.csv")
+coviddata <- read_csv("United_States_COVID-19_Cases_and_Deaths_by_State_over_Time.csv")
 states <- read_csv("states")
 colnames(states) <- c("StateName", "Abbreviation")
-casesstates <- left_join(cases, states, by = c("state" = "Abbreviation"))
-cases$datescases <- as.Date(cases$submission_date, format = "%m/%d/%Y")
-uptodatecases <- cases %>% group_by(state) %>% 
-  filter(datescases == max(datescases))
-totalcasesdata = uptodatecases[,c ('state', 'tot_death')]
+datastates <- left_join(coviddata, states, by = c("state" = "Abbreviation"))
+datastates$datadates <- as.Date(coviddata$submission_date, format = "%m/%d/%Y")
+totalcoviddata <- datastates %>% group_by(state) %>% 
+  filter(datadates == max(datadates))
 
-deaths <- read_csv("United_States_COVID-19_Cases_and_Deaths_by_State_over_Time.csv")
-states <- read_csv("states")
-colnames(states) <- c("StateName", "Abbreviation")
-deathsstates <- left_join(deaths, states, by = c("state" = "Abbreviation"))
-deaths$datesdeaths <- as.Date(deaths$submission_date, format = "%m/%d/%Y")
-uptodatedeaths <- deaths %>% group_by(state) %>% 
-  filter(datesdeaths == max(datesdeaths))
-totaldeathsdata = uptodatedeaths[,c ('state', 'tot_death')]
 
 dataexplorerpage <- 
   fluidPage(
